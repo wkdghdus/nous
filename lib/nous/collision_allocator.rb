9c7203c62b265f66d75c98111126eb2d288d7298
Hoyeon Luke Jang <h.lukejang@gmail.com>
2026-09-10 00:54:02 -0400

    refactor(nous-core): support candidate collision allocation

--- lib/nous/collision_allocator.rb
+++ lib/nous/collision_allocator.rb
@@ -4,15 +4,17 @@ module Nous
   module CollisionAllocator
     module_function
 
-    def next_available_path(vault_root:, directory:, basename:, extension: ".md")
+    def next_available_path(vault_root:, directory:, basename:, extension: ".md", reserved_ids: [])
       root = PathGuard.validate_vault_root(vault_root)
+      reserved = reserved_ids.each_with_object({}) { |id, values| values[id.to_s] = true }
       index = nil
 
       loop do
         filename = index.nil? ? "#{basename}#{extension}" : "#{basename}-#{index}#{extension}"
         relative = "#{directory}/#{filename}"
         path = PathGuard.internal_path(vault_root: root, path: relative, allow_missing: true, create_parent: true)
-        return { path: path, relative_path: relative, id: File.basename(filename, extension) } unless path.exist?
+        id = File.basename(filename, extension)
+        return { path: path, relative_path: relative, id: id } unless path.exist? || reserved.key?(id)
 
         index = index.nil? ? 2 : index + 1
       end
